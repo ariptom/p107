9abe436bc1c7d28e948ad91a9210f1deaa1d2c7d
ariptom <95114854+ariptom@users.noreply.github.com>
2022-11-11 19:14:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function startClassification(){
     navigator.mediaDevices.getUserMedia({audio:true});
     classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/7oxhNxcJPA/model.json',modelReady);
 }
-function modelReady {
+function modelReady () {
     classifier.classify(gotResults);
 }
 function gotResults (error,results){
